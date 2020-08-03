d63f441cf86999b70340626ba49656bbcfe19042
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-03 09:58:53 +0530

    Create symmetricpairs.py

---
+++ symmetricpairs.py
@@ -0,0 +1,33 @@
+"""
+
+Two pairs (a, b) and (c, d) are said to be symmetric if c is equal to b and a is equal to d. 
+
+For example, (10, 20) and (20, 10) are symmetric. Given an array of n pairs find all symmetric pairs in it.
+
+It may be assumed that the first elements of all pairs are distinct.
+
+Constraints:
+2<=n<=1000
+
+Input:
+Firstline indicates integer n
+Next n lines indicates the pairs of elements
+
+Output:
+Print all the symmetric pairs
+
+Example:
+
+Input:
+5
+11 20
+30 40
+5 10
+40 30
+10 5
+
+Output:
+30 40
+5 10
+
+"""
