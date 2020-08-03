e04a7de2ebe6b89d1b075c907b24b8084320cf25
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-03 09:48:15 +0530

    Create largestsubsetwith fibonacci numbers.py

---
+++ largestsubsetwith fibonacci numbers.py
@@ -0,0 +1,27 @@
+"""
+Given an array with n positive numbers.The task is that we find largest subset from array that contain elements which are Fibonacci numbers.
+
+Input:
+Firstline indicates the integer n
+secondline indicates the elements of array
+
+Output:
+print the largest subarray with fibonacci numbers
+
+Examples :
+
+Input :
+1 4 3 9 10 13 7
+
+Output :
+1 3 13
+
+Explanation:
+The output three numbers are Fibonacci numbers.
+
+Input  :
+0 2 8 5 2 1 4 13 23
+
+Output :
+0 2 8 5 2 1 13 23
+"""
