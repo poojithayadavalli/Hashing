81b43e0543269a1e4e38e72dcabf44a71aa1dcd9
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-11 11:30:34 +0530

    Update largestsubset with sumofeverypair is prime.py

--- largestsubset with sumofeverypair is prime.py
+++ largestsubset with sumofeverypair is prime.py
@@ -1,6 +1,8 @@
 """
 
-Given an array A[], find a subset of maximum size in which sum of every pair of elements is a prime number. Print its length and the subset. Consider many queries for different arrays and maximum value of an element as 100000.
+Given an array A[], find a subset of maximum size in which sum of every pair of elements is a prime number.
+
+Print its length and the subset. Consider many queries for different arrays and maximum value of an element as 100000.
 
 Examples :
 
@@ -36,12 +38,16 @@ import math as mt
 MAX = 100001
   
 isPrime = [0 for i in range(MAX)] 
-def sieve():
+  
+def sieve(): 
+  
     for p in range(2, mt.ceil(mt.sqrt(MAX))):
         if (isPrime[p] == 0) :
             for i in range(2 * p, MAX, p): 
                 isPrime[i] = 1
-def findSubset(a, n):
+  
+def findSubset(a, n): 
+  
     cnt1 = 0
     for i in range(n):  
         if (a[i] == 1): 
@@ -52,26 +58,30 @@ def findSubset(a, n):
             if ((a[i] != 1) and
                 (isPrime[a[i] + 1] == 0)): 
   
-                print(cnt1 + 1)  
+                print(cnt1 + 1) 
                 for j in range(cnt1): 
                     print("1", end = " ") 
   
                 print(a[i]) 
-                return 
+                return 0 
     if (cnt1 >= 2): 
   
-        print(cnt1)
-        for i in range(cnt1-1): 
+        print(cnt1) 
+  
+        # Print all ones(1s). 
+        for i in range(cnt1): 
             print("1", end = " ") 
   
-        print("1") 
-        return 
+        print() 
+        return 0
     for i in range(n): 
-        for j in range(i + 1, n):
+        for j in range(i + 1, n): 
             if (isPrime[a[i] + a[j]] == 0): 
                 print(2) 
-                print(a[i], " ", a[j])
-    print(-1)
+                print(a[i], " ", a[j]) 
+    print(-1) 
+sieve()
 n=int(input())
 arr=list(map(int,input().split()))
 findSubset(arr,n)
+
